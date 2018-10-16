08e39c6526e86108690c906c7ecbdb336ba4e352
William N. Roney <wroney@roneymis.com>
2018-10-16 10:34:22 -0400

    Added lines for the builds

--- build.sh
+++ build.sh
@@ -5,4 +5,8 @@
 # runs the build and pushes to hub.docker.com
 #
 
-git clone https://github.com/jenksci/docker
+git clone https://github.com/jenkinsci/docker
+cd docker
+docker build -t wroney688/rpi-jenkins:latest -f Dockerfile .
+docker build -t wroney688/rpi-jenkins:latest-alpine -f Dockerfile-alpine .
+docker build -t wroney688/rpi-jenkins:latest-slim -f Dockerfile-slim .
