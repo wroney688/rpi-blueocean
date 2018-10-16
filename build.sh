1953db06757230e02f38dac544b3ea1272989655
William N. Roney <wroney@roneymis.com>
2018-10-16 10:37:09 -0400

    fixed tag

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@
 
 git clone https://github.com/jenkinsci/docker
 cd docker
-docker build -t wroney688/rpi-jenkins:latest -f Dockerfile .
-docker build -t wroney688/rpi-jenkins:latest-alpine -f Dockerfile-alpine .
-docker build -t wroney688/rpi-jenkins:latest-slim -f Dockerfile-slim .
+docker build -t wroney/rpi-jenkins:latest -f Dockerfile .
+docker build -t wroney/rpi-jenkins:latest-alpine -f Dockerfile-alpine .
+docker build -t wroney/rpi-jenkins:latest-slim -f Dockerfile-slim .
