9efee2b52eeb1bbd0dd7d3382b3babd01e0d07d5
Dr. William N Roney <33969189+wroney688@users.noreply.github.com>
2018-10-23 12:11:55 -0400

    Multi-arch label change

--- build.sh
+++ build.sh
@@ -5,11 +5,51 @@
 # runs the build and pushes to hub.docker.com
 #
 
-git clone https://github.com/jenkinsci/docker
-cd docker
-docker build -t wroney/rpi-jenkins:`arch`-latest -f Dockerfile .
-docker push wroney/rpi-jenkins:`arch`-latest
-docker build -t wroney/rpi-jenkins:`arch`-latest-alpine -f Dockerfile-alpine .
-docker push wroney/rpi-jenkins:`arch`-latest-alpine
-docker build -t wroney/rpi-jenkins:`arch`-latest-slim -f Dockerfile-slim .
-docker push wroney/rpi-jenkins:`arch`-latest-slim
+ARCH=`arch`
+echo -e "\033[1;35mPlatform is $ARCH\033[0m"
+if [ -d docker ]
+then
+        echo -e "\033[1;36mUpdating jenkinsci/docker\033[0m"
+        cd docker
+        git pull origin
+else
+        echo -e "\033[1;36mRetrieving jenkinsci/docker\033[0m"
+        git clone https://github.com/jenksci/docker
+        cd docker
+fi
+echo -e "\033[1;36mBuilding latest\033[0m"
+docker build -t wroney/rpi-jenkins:latest -f Dockerfile .
+echo -e "\033[1;36mBuilding latest-slim\033[0m"
+docker build -t wroney/rpi-jenkins:latest-slim -f Dockerfile-slim .
+echo -e "\033[1;36mBuilding latest-alpine\033[0m"
+docker build -t wroney/rpi-jenkins:latest-alpine -f Dockerfile-alpine .
+
+if [ "$ARCH" = "arm" ]
+then
+        echo -e "\033[1;34mTagging for ARCH:armv7\033[0m"
+        docker tag wroney/rpi-jenkins:latest wroney/rpi-jenkins-arm:latest
+        docker tag wroney/rpi-jenkins:latest-slim wroney/rpi-jenkins-arm:latest-slim
+        docker tag wroney/rpi-jenkins:latest-alpine wroney/rpi-jenkins-arm:latest-alpine
+        echo -e "\033[1;34mPushing for ARCH:armv7\033[0m"
+        docker push wroney/rpi-jenkins-arm:latest
+        docker push wroney/rpi-jenkins-arm:latest-slim
+        docker push wroney/rpi-jenkins-arm:latest-alpine
+elif [ "$ARCH" = "aarch64" -0 "$ARCH" = "arm64" ]
+then
+        echo -e "\033[1;34mTagging for ARCH:armv8\033[0m"
+        docker tag wroney/rpi-jenkins:latest wroney/rpi-jenkins-arm64:latest
+        docker tag wroney/rpi-jenkins:latest-slim wroney/rpi-jenkins-arm64:latest-slim
+        docker tag wroney/rpi-jenkins:latest-alpine wroney/rpi-jenkins-arm64:latest-alpine
+        docker tag wroney/rpi-jenkins:latest wroney/rpi-jenkins-aarch64:latest
+        docker tag wroney/rpi-jenkins:latest-slim wroney/rpi-jenkins-aarch64:latest-slim
+        docker tag wroney/rpi-jenkins:latest-alpine wroney/rpi-jenkins-aarch64:latest-alpine
+        echo -e "\033[1;34mPushing for ARCH:armv7\033[0m"
+        docker push wroney/rpi-jenkins-arm64:latest
+        docker push wroney/rpi-jenkins-arm64:latest-slim
+        docker push wroney/rpi-jenkins-arm64:latest-alpine
+        docker push wroney/rpi-jenkins-aarch64:latest
+        docker push wroney/rpi-jenkins-aarch64:latest-slim
+        docker push wroney/rpi-jenkins-aarch64:latest-alpine
+else
+        echo -e "\033[1;34mUnknown architecture, bailing.\033[0m"
+fi
